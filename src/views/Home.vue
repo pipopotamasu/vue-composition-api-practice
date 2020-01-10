ee591e7904082623f88866459f7d40b0607d1f53
mpg-yamato-murakami <yamato.murakami@medpeer.co.jp>
2020-01-10 13:30:26 +0900

    before using composition api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" name="text-iuput" v-model="value" />
+    <div>
+      <span>{{ value }}</span>
+      <button @click="onSubmit">submit</button>
+      <button @click="onClear">clear</button>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import Vue from 'vue'
 
-export default {
+export default Vue.extend({
   name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (!this.value) return;
+      alert(this.value)
+    },
+    onClear () {
+      this.value = ''
+    }
   }
-}
+})
 </script>
